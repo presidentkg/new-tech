4d23cd6ed0833a014455cd25cf6c339609fe3f8e
Anders Johansson <inboxanders@gmail.com>
2025-09-10 12:25:44 +0200

    Changed the layout for the contact page and improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default async function Home() {
           {/* <ProductList products={smartphones} /> */}
           <ProductList products={laptops} />
         </div>
+      </main>
     </div>
   );
 }
