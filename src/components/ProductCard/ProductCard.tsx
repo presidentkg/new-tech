630afa6339d31b5291801d2d756597319d4a612f
Karl Ragnar <karlragnar.kling@gmail.com>
2025-09-12 09:23:37 +0200

    Added Link to card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,13 +2,14 @@ import { Product } from "@/lib/utils/interface";
 import styles from "./ProductCard.module.css";
 import Image from "next/image";
 import StockStatus from "../StockStatus/StockStatus";
+import Link from "next/link";
 
 export default function ProductCard({product} : {product: Product}) {
     const originalPrice = (product.price / (1 - product.discountPercentage / 100)).toFixed(2);
     const discount = (parseFloat(originalPrice) - product.price).toFixed(2);
     return (
-        <section className={styles.card}>
-            <div className={styles.image}>
+        <Link href={`/products/${product.id}`} className={styles.card}>
+            <header className={styles.image}>
                 <Image
                     src={product.images[0]}
                     alt={"product image"}
@@ -16,17 +17,19 @@ export default function ProductCard({product} : {product: Product}) {
                     height={150}
                 />
                 <div className={styles.saleBadge}>SALE!</div>
-            </div>
-            <h3>{product.title}</h3>
-            <p>{product.description}</p>
-            <div className={styles.priceWrapper}>
+            </header>
+            <section>
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+            </section>
+            <footer className={styles.priceWrapper}>
                 <p className={styles.price}>{product.price} :-</p>
                 <div className={styles.pricingDetails}>
                     <p className={styles.save}>Save {discount} :-</p>
                     <p className={styles.originalPrice}>Original price: {originalPrice} :-</p> 
                 </div>
-            </div>
+            </footer>
             <StockStatus stock={product.stock} />
-        </section>
+        </Link>
     )
 }
