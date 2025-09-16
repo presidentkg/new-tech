ef189b3029d1519f0ec3266d495d9fd1472bb07e
Karl Ragnar <karlragnar.kling@gmail.com>
2025-09-16 12:42:20 +0200

    Created Price component

---
+++ src/components/Price/Price.tsx
@@ -0,0 +1,17 @@
+import { Product } from "@/lib/utils/interface";
+import styles from './Price.module.css';
+
+
+export default function Price({product} : {product : Product}){
+    const originalPrice = (product.price / (1 - product.discountPercentage / 100)).toFixed(2);
+    const discount = (parseFloat(originalPrice) - product.price).toFixed(2);
+    return(
+        <section className={styles.priceWrapper}>
+            <span className={styles.price}>{product.price} :-</span>
+            <div className={styles.pricingDetails}>
+                <p className={styles.save}>Save {discount} :-</p>
+                <span className={styles.originalPrice}>Original price: {originalPrice} :-</span> 
+            </div>
+        </section>
+    )
+}
