01bb03d61860795f6d311a7c3d0bd806430e756e
Karl Ragnar <karlragnar.kling@gmail.com>
2025-09-25 10:38:22 +0200

    Added useActionState, added an alert and changed Cancel from Link to button

--- src/components/UpdateForm/UpdateForm.tsx
+++ src/components/UpdateForm/UpdateForm.tsx
@@ -1,13 +1,27 @@
+"use client"
+
 import { UpdateAction } from "@/app/actions/products";
 import { Product } from "@/lib/utils/interface";
 import Form from "next/form"
-import Link from "next/link";
 import styles from "./UpdateForm.module.css";
+import { useActionState } from "react";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 
 export default function UpdateForm({product} : {product : Product}){
+    const [state, formAction, isPending] = useActionState(UpdateAction, {message: ""})
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if (state.message){
+            alert(`Old title: ${product.title}, Old price: ${product.price}\n${state.message}`);
+            router.push("/admin");
+        }
+    }, [state.message]);
+  
     return(
-        <Form action={UpdateAction} className={styles.updateForm}>
+        <Form action={formAction} className={styles.updateForm}>
             <h3>Uppdatera <span className={styles.title}>{product.title}</span></h3>
             <div className={styles.inputGroup}>
                 <label htmlFor="title">New title: </label>
@@ -26,16 +40,25 @@ export default function UpdateForm({product} : {product : Product}){
                     name="price"
                     id="price"
                     defaultValue={product.price}
+                    min={0}
+                    step={0.01}
                 />
             </div>
             <div className={styles.buttons}>
-                <button type="submit" className={styles.button}>Save</button>
-                <Link
+                <button 
+                    type="submit"
+                    className={styles.button}
+                    disabled={isPending}
+                >
+                    {isPending ? "Updating..." : "Update"}
+                </button>
+                <button
                     className={`${styles.button} ${styles.cancel}`}
-                    href="/admin"
+                    onClick={() => router.push("/admin")}
+                    disabled={isPending}
                 >
                     Cancel
-                </Link>
+                </button>
             </div>
         </Form>
     )
